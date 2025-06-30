317f2e0847cb314daca16461ccf8da6933daf360
yunsangee <michael0622@naver.com>
2025-06-30 19:58:22 +0900

    '전적검색'

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-// router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/components/Login.vue'
 import MainPage from '@/components/MainPage.vue'
 import SignupPage from '@/components/SignupPage.vue'
 import GoogleRedirect from '@/components/GoogleLoginRedirect.vue'
+import MatchDetail from '@/components/MatchDetail.vue'
 
 const routes = [
   { path: '/', component: MainPage },
   { path: '/login', component: LoginPage },
   { path: '/signup', component: SignupPage },
-  { path: '/redirect', component: GoogleRedirect }
+  { path: '/redirect', component: GoogleRedirect },
+
+  {
+    path: '/matchdetail/:gameName/:tagLine',
+    name: 'MatchDetail',
+    component: MatchDetail
+  }
 ]
 
 const router = createRouter({
