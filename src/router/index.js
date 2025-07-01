8e97902fbcf77e05e6c0ade5684b01c771269478
sungnam cho <sung6081@gmail.com>
2025-07-01 16:11:44 +0900

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,9 @@ import LoginPage from '@/components/Login.vue'
 import MainPage from '@/components/MainPage.vue'
 import SignupPage from '@/components/SignupPage.vue'
 import GoogleRedirect from '@/components/GoogleLoginRedirect.vue'
-import MatchDetail from '@/components/MatchDetail.vue'
-<<<<<<<<< Temporary merge branch 1
 import SearchResult from '@/components/SearchResult.vue'
 import NotFound from '@/components/NotFound.vue'
-=========
 import MatchDetail from '@/components/MatchDetail.vue'
->>>>>>>>> Temporary merge branch 2
 
 const routes = [
   { path: '/', component: MainPage },
